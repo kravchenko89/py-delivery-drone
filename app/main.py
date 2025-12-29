0d11378f057b11a0c375d9bff3fd7a29f429cf18
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2025-12-29 16:12:44 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
@@ -49,7 +52,7 @@ class DeliveryDrone(FlyingRobot):
         weight: int,
         coords: list = None,
         max_load_weight: int = 0,
-        current_load: int = None
+        current_load: Optional[Cargo] = None
     ) -> None:
 
         super().__init__(name, weight, coords)
@@ -60,9 +63,12 @@ class DeliveryDrone(FlyingRobot):
         if current_load is not None:
             self.hook_load(current_load)
 
-    def hook_load(self, cargo: int) -> None:
-        if self.current_load is None and self.weight <= self.max_load_weight:
-            self.current_load = cargo
+    def hook_load(self, cargo: Cargo) -> None:
+        # проверяем что current_load пустой и вес не превышает max_load_weight
+        if self.current_load is None:
+            cargo_weight = getattr(cargo, "weight", 0)
+            if cargo_weight <= self.max_load_weight:
+                self.current_load = cargo
 
     def unhook_load(self) -> None:
         self.current_load = None
